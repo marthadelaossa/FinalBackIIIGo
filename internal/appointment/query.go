507a6620fa17197bac51b83659c14d74af356801
Ismael Carvajal <ismael.carvajal@mercurio.com.co>
2023-12-04 20:57:18 -0500

    Cambio variables del .env, cambio linea del main y corrijo query de turnos

--- internal/appointment/query.go
+++ internal/appointment/query.go
@@ -1,10 +1,10 @@
 package appointment
 
 const (
-	QueryInsertAppointment  = `INSERT INTO clinicaodontologica.appointments(description, id_odontologo, id_paciente, date_time) VALUES (?, ?, ?, ?)`
-	QueryGetAllAppointments = `SELECT id, description, id_odontologo, id_paciente, date_time FROM clinicaodontologica.appointments`
-	QueryDeleteAppointment  = `DELETE FROM clinicaodontologica.appointments WHERE id = ?`
-	QueryGetAppointmentById = `SELECT id, description, id_odontologo, id_paciente, date_time FROM clinicaodontologica.appointments WHERE id = ?`
-	QueryUpdateAppointment  = `UPDATE clinicaodontologica.appointments SET description = ?, id_odontologo = ?, id_paciente = ?, date_time = ? WHERE id = ?`
-	QueryPatchAppointment   = `UPDATE clinicaodontologica.appointments SET description = COALESCE(?, description), id_odontologo = COALESCE(?, id_odontologo), id_paciente = COALESCE(?, id_paciente), date_time = COALESCE(?, date_time) WHERE id = ?`
+	QueryInsertAppointment  = `INSERT INTO clinicaodontologica.turnos(description, id_odontologo, id_paciente, date_time) VALUES (?, ?, ?, ?)`
+	QueryGetAllAppointments = `SELECT id, description, id_odontologo, id_paciente, date_time FROM clinicaodontologica.turnos`
+	QueryDeleteAppointment  = `DELETE FROM clinicaodontologica.turnos WHERE id = ?`
+	QueryGetAppointmentById = `SELECT id, description, id_odontologo, id_paciente, date_time FROM clinicaodontologica.turnos WHERE id = ?`
+	QueryUpdateAppointment  = `UPDATE clinicaodontologica.turnos SET description = ?, id_odontologo = ?, id_paciente = ?, date_time = ? WHERE id = ?`
+	QueryPatchAppointment   = `UPDATE clinicaodontologica.turnos SET description = COALESCE(?, description), id_odontologo = COALESCE(?, id_odontologo), id_paciente = COALESCE(?, id_paciente), date_time = COALESCE(?, date_time) WHERE id = ?`
 )
